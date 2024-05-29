36b8be02d6f3eebab83c385553ffe78e810bc265
Musharraf <musharrafjamal92@gmail.com>
2024-05-29 17:54:55 +0530

    Worked on shipping page

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -3,80 +3,24 @@ import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 import { Button, ButtonGroup } from "@material-tailwind/react";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { RiErrorWarningFill } from "react-icons/ri";
 import { RxCross1, RxCross2 } from "react-icons/rx";
+import { VscDebugContinue } from "react-icons/vsc";
 
 const Cart = () => {
   const router = useRouter();
-  const [products, setProducts] = useState([
-    {
-      name: "Breathable slim sport vest",
-      color: "Blue",
-      size: "S",
-      price: 59.0,
-      quantity: 2,
-      image:
-        "https://img.freepik.com/free-photo/medium-shot-smiley-man-posing_23-2149915892.jpg?t=st=1716364756~exp=1716368356~hmac=6861a2a425d375a48ca9f795b11f41a4f960f25bad703b6898d9b2959c3d23ca&w=360",
-    },
-    {
-      name: "Breathable slim sport vest",
-      color: "Pink",
-      size: "S",
-      price: 39.0,
-      quantity: 1,
-      image:
-        "https://img.freepik.com/free-photo/beautiful-male-half-length-portrait-isolated-white-studio-background-young-emotional-hindu-man-blue-shirt-facial-expression-human-emotions-advertising-concept-standing-smiling_155003-25250.jpg?t=st=1716364719~exp=1716368319~hmac=546ca7bf121267955fa0beaa40b7945c65ddc621f33d8bbe5734070783a628cf&w=996",
-    },
-    {
-      name: "Breathable slim sport vest",
-      color: "Brown",
-      size: "S",
-      price: 39.0,
-      quantity: 1,
-      image:
-        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      name: "Breathable slim sport vest",
-      color: "Brown",
-      size: "S",
-      price: 39.0,
-      quantity: 1,
-      image:
-        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      name: "Breathable slim sport vest",
-      color: "Brown",
-      size: "S",
-      price: 39.0,
-      quantity: 1,
-      image:
-        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      name: "Breathable slim sport vest",
-      color: "Brown",
-      size: "S",
-      price: 39.0,
-      quantity: 1,
-      image:
-        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      name: "Breathable slim sport vest",
-      color: "Brown",
-      size: "S",
-      price: 39.0,
-      quantity: 1,
-      image:
-        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ]);
-
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    if (cart) {
+      setProducts(cart);
+    }
+    console.log(cart);
+  }, []);
   const [validationMessage, setValidationMessage] = useState(false);
 
   const handleQuantityChange = (index, change) => {
@@ -96,12 +40,23 @@ const Cart = () => {
         }
         return product;
       });
+
+      // Update localStorage with the updated products
+      localStorage.setItem("cart", JSON.stringify(updatedProducts));
+
       return updatedProducts;
     });
   };
 
   const handleRemoveProduct = (index) => {
-    setProducts((prevProducts) => prevProducts.filter((_, i) => i !== index));
+    setProducts((prevProducts) => {
+      const updatedProducts = prevProducts.filter((_, i) => i !== index);
+
+      // Update localStorage with the updated products
+      localStorage.setItem("cart", JSON.stringify(updatedProducts));
+
+      return updatedProducts;
+    });
   };
 
   return (
@@ -111,86 +66,120 @@ const Cart = () => {
       </Head>
       <Nav />
       <div className="flex flex-col lg:flex-row px-4 lg:px-10 h-full justify-between gap-6 lg:gap-10 my-6 lg:my-10">
-      <div className="w-full lg:w-2/3 min-h-auto max-h-[78vh] no-scrollbar overflow-auto bg-white pb-6 shadow-lg rounded-lg">
-        <div className="flex items-center justify-between gap-1 cursor-pointer text-gray-700 mb-6 sticky px-6 py-4 top-0 bg-white">
-          <div className="flex items-center gap-1 cursor-pointer" onClick={() => router.back()}>
-            <FaArrowLeftLong />
-            <div>Cart</div>
-          </div>
-          <div className={`text-red-500 flex items-center gap-1 transition-all duration-700 ${validationMessage ? "translate-x-0" : "translate-x-96"}`}>
-            <RiErrorWarningFill />
-            Quantity should be between 1 and 10.
-          </div>
-        </div>
-        <div className="mt-4 space-y-6 px-6">
-          {products.map((product, index) => (
-            <div key={index} className="flex flex-col lg:flex-row items-start lg:items-center justify-between border-b pb-4">
-              <div className="flex items-start lg:items-center">
-                <img src={product.image} alt={product.name} className="w-20 h-20 object-cover rounded" />
-                <div className="ml-4">
-                  <div className="font-semibold">{product.name}</div>
-                  <div className="text-gray-500 text-sm">Color: {product.color} / Size: {product.size}</div>
-                  <div className="text-gray-500 text-sm">₹{product.price.toFixed(2)}</div>
-                </div>
-              </div>
-              <div className="flex w-full lg:w-max flex-col lg:flex-row gap-4 mt-4 lg:mt-0">
-                <ButtonGroup variant="gradient" color="light-blue" size="sm">
-                  <Button onClick={() => handleQuantityChange(index, -1)}>-</Button>
-                  <div className="px-4 flex items-center">{product.quantity}</div>
-                  <Button onClick={() => handleQuantityChange(index, 1)}>+</Button>
-                </ButtonGroup>
-              </div>
-              <div className="font-semibold mt-4 lg:mt-0">₹{(product.price * product.quantity).toFixed(2)}</div>
-              <button
-                onClick={() => handleRemoveProduct(index)}
-                className="bg-[#FA7070] hover:bg-[#FD5D5D] shadow-lg shadow-[#ffb6b69a] text-white px-4 py-2 rounded-md flex items-center justify-center gap-1 transition-all mt-4 lg:mt-0"
-                title="Remove Service"
-              >
-               Remove service 
-              </button>
+        <div className="w-full lg:w-2/3 min-h-auto max-h-[78vh] no-scrollbar overflow-auto bg-white pb-6 shadow-lg rounded-lg">
+          <div className="flex items-center justify-between gap-1 cursor-pointer text-gray-700 mb-6 sticky px-6 py-4 top-0 bg-white">
+            <div
+              className="flex items-center gap-1 cursor-pointer"
+              onClick={() => router.back()}
+            >
+              <FaArrowLeftLong />
+              <div>Cart</div>
             </div>
-          ))}
+            <div
+              className={`text-red-500 flex items-center gap-1 transition-all duration-700 ${
+                validationMessage ? "translate-x-0" : "translate-x-96"
+              }`}
+            >
+              <RiErrorWarningFill />
+              Quantity should be between 1 and 10.
+            </div>
+          </div>
+          <div className="mt-4 space-y-6 px-6">
+            {products.map((product, index) => (
+              <div
+                key={index}
+                className="flex flex-col lg:flex-row items-start lg:items-center justify-between border-b pb-4"
+              >
+                <div className="flex items-start lg:items-center">
+                  <img
+                    src={product.icon?.url}
+                    alt={product.name}
+                    className="w-20 h-20 object-cover rounded"
+                  />
+                  <div className="ml-4">
+                    <div className="font-semibold">{product.name}</div>
+                    <div className="text-gray-500 text-sm">
+                      ₹{parseFloat(product.price).toFixed(2)}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex w-full lg:w-max flex-col lg:flex-row gap-4 mt-4 lg:mt-0">
+                  <ButtonGroup variant="gradient" color="light-blue" size="sm">
+                    <Button onClick={() => handleQuantityChange(index, -1)}>
+                      -
+                    </Button>
+                    <div className="px-4 flex items-center">
+                      {product.quantity}
+                    </div>
+                    <Button onClick={() => handleQuantityChange(index, 1)}>
+                      +
+                    </Button>
+                  </ButtonGroup>
+                </div>
+                <div className="font-semibold mt-4 lg:mt-0">
+                  ₹{(product.price * product.quantity).toFixed(2)}
+                </div>
+                <button
+                  onClick={() => handleRemoveProduct(index)}
+                  className="bg-[#FA7070] hover:bg-[#FD5D5D] shadow-lg shadow-[#ffb6b69a] text-white px-4 py-2 rounded-md flex items-center justify-center gap-1 transition-all mt-4 lg:mt-0"
+                  title="Remove Service"
+                >
+                  Remove service
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-full lg:w-1/3 h-full bg-white p-6 shadow-lg rounded-lg">
+          <h2 className="text-lg font-semibold border-b pb-4">Summary</h2>
+          <div className="mt-4 space-y-6">
+            <div className="flex justify-between">
+              <span>Subtotal</span>
+              <span>
+                ₹
+                {products
+                  .reduce(
+                    (acc, product) => acc + product.price * product.quantity,
+                    0
+                  )
+                  .toFixed(2)}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span>Convenience Fee</span>
+              <span>₹18.00</span>
+            </div>
+            <div className="flex justify-between font-semibold text-lg">
+              <span>Total</span>
+              <span>
+                ₹
+                {(
+                  products.reduce(
+                    (acc, product) => acc + product.price * product.quantity,
+                    0
+                  ) + 18
+                ).toFixed(2)}
+              </span>
+            </div>
+            <Link href={"/shipping"}>
+              <button className="w-full py-3 mt-3 flex items-center gap-1 hover:gap-2 justify-center hover:bg-gray-800 transition-all text-white rounded-lg bg-gray-700">
+                Continue <VscDebugContinue />
+              </button>
+            </Link>
+            <div className="text-sm text-gray-500">
+              <p className="text-base">About Service Providers</p>
+              <p>
+                Our service providers are hihgly qualified and designed to
+                provide customers with a variety of services.
+              </p>
+            </div>
+            <div className="text-sm text-gray-500">
+              <p>NEED HELP?</p>
+              <p>Contact our support team for assistance.</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-full lg:w-1/3 h-full bg-white p-6 shadow-lg rounded-lg">
-        <h2 className="text-lg font-semibold border-b pb-4">Summary</h2>
-        <div className="mt-4 space-y-6">
-          <div className="flex justify-between">
-            <span>Subtotal</span>
-            <span>
-              ₹
-              {products.reduce((acc, product) => acc + product.price * product.quantity, 0).toFixed(2)}
-            </span>
-          </div>
-          <div className="flex justify-between">
-            <span>International Shipping</span>
-            <span>₹18.00</span>
-          </div>
-          <div className="flex justify-between font-semibold text-lg">
-            <span>Total</span>
-            <span>
-              ₹
-              {(products.reduce((acc, product) => acc + product.price * product.quantity, 0) + 18).toFixed(2)}
-            </span>
-          </div>
-          <button className="w-full py-3 hover:bg-gray-800 transition-all text-white rounded-lg bg-gray-700">
-            CHECK OUT
-          </button>
-          <button className="w-full py-3 border rounded-lg flex items-center justify-center space-x-2">
-            <img src="/paypal-logo.png" alt="PayPal" className="w-6" />
-            <span>Check out with PayPal</span>
-          </button>
-          <div className="text-sm text-gray-500">
-            <p>FREE SHIPPING</p>
-            <p>Your order qualifies for free shipping. Join us for free shipping on every order, every time.</p>
-          </div>
-          <div className="text-sm text-gray-500">
-            <p>NEED HELP?</p>
-            <p>Contact our support team for assistance.</p>
-          </div>
-        </div>
-      </div>
-    </div>
       <Footer />
     </div>
   );
--- app/shipping/page.jsx
+++ app/shipping/page.jsx
@@ -2,77 +2,262 @@
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 import { IoMdInformationCircleOutline } from "react-icons/io";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Input,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Radio,
+  Textarea,
+  Typography,
+} from "@material-tailwind/react";
 
 function Shipping() {
-  // Initialize state variables
   const [formData, setFormData] = useState({
-    fullName: '',
-    phone: '',
-    address: '',
-    date: '',
-    time: ''
+    fullname: "",
+    phoneNumber: "",
+    address: "",
+    date: "",
+    time: "",
   });
+
+  const [dates, setDates] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   // Create handler functions
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission
+    if (formData.time <= getCurrentTime()) {
+      alert("Please select a time after the current time");
+      return;
+    }
     console.log(formData);
   };
+
+  // Function to generate the four days including the current date
+  const getFourDays = () => {
+    const today = new Date();
+    const fourDaysArray = [today];
+
+    for (let i = 1; i < 4; i++) {
+      const nextDate = new Date(today);
+      nextDate.setDate(today.getDate() + i);
+      fourDaysArray.push(nextDate);
+    }
+
+    return fourDaysArray.map((date) => {
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const year = date.getFullYear();
+      return `${day}-${month}-${year}`;
+    });
+  };
+
+  const getCurrentTime = () => {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    return `${hours}:${minutes}`;
+  };
+
+  const getAddress = async ({ lat, lng }) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
+      );
+      const data = await response.json();
+      if (data.results && data.results.length > 0) {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          address: data.results[0].formatted_address,
+        }));
+      } else {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          address: "Address not found",
+        }));
+      }
+    } catch (error) {
+      console.error("Error fetching address:", error);
+    }
+  };
+
+  const gettingUser = async () => {
+    const id = localStorage.getItem("token");
+    const response = await fetch(`/api/users/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      fullname: data.name || "Name",
+      phoneNumber: data.phoneNumber || "Phone Number",
+      email: data.email || "Email",
+    }));
+  };
+
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(cart);
+    gettingUser();
+    const dates = getFourDays();
+    setDates(dates);
+    const location = JSON.parse(localStorage.getItem("location"));
+    getAddress(location);
+
+    // Set the initial date in formData
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      date: dates[0],
+    }));
+  }, []);
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
 
   return (
     <div>
       <Nav />
-      <div className="flex flex-col md:flex-row justify-between p-8">
-        <div className="w-full md:w-2/3 pr-0 md:pr-8">
+      <div className="flex flex-col md:flex-row justify-center gap-20 p-8">
+        <div className="w-full md:w-1/3">
           <h2 className="font-julius lg:text-4xl md:text-4xl sm:text-3xl text-3xl mb-4 text-gray-700">
-            01. SHIPPING
+            SUMMARY
+          </h2>
+          <div className="space-y-4">
+            {cartItems.map((item, index) => (
+              <div key={index} className="flex items-center">
+                <img
+                  src={item.icon?.url}
+                  alt="Product"
+                  className="w-24 h-24 mr-3 object-cover rounded-lg"
+                />
+                <div>
+                  <h3 className="font-bold">{item.name}</h3>
+                  <p className="text-sm">Qty: {item.quantity}</p>
+                  <p className="text-sm font-bold text-teal-500">
+                    ₹{item.price}.00
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="h-px w-full bg-gray-300 mt-4"></div>
+
+          <div className="flex flex-col gap-4 p-2">
+            <div className="flex justify-between">
+              <span>Subtotal</span>
+              <span>
+                ₹
+                {cartItems
+                  .reduce(
+                    (acc, product) => acc + product.price * product.quantity,
+                    0
+                  )
+                  .toFixed(2)}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span>Convenience Fee</span>
+              <span>₹18.00</span>
+            </div>
+            <div className="flex justify-between font-bold text-xl text-gray-700">
+              <span>Total</span>
+              <span>
+                ₹
+                {(
+                  cartItems.reduce(
+                    (acc, product) => acc + product.price * product.quantity,
+                    0
+                  ) + 18
+                ).toFixed(2)}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 bg-white p-6 rounded-xl shadow-lg">
+          <h2 className="font-julius text-center lg:text-4xl md:text-4xl sm:text-3xl text-3xl mb-4 text-gray-700">
+            SHIPPING
           </h2>
           <form className="space-y-4" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="fullName"
-              placeholder="Full name"
-              value={formData.fullName}
+            <Input
+              label="Full name"
+              className="bg-white"
+              name="fullname"
+              value={formData.fullname}
               onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded"
+              required
+              maxLength={30}
+              minLength={4}
             />
-            <input
-              type="text"
-              name="phone"
-              placeholder="Phone number"
-              value={formData.phone}
+            <Input
+              label="Phone number"
+              className="bg-white"
+              name="phoneNumber"
+              value={formData.phoneNumber}
               onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded"
+              required
+              maxLength={10}
+              minLength={10}
             />
-            <input
-              type="text"
+            <Textarea
+              label="Address"
+              className="bg-white"
               name="address"
-              placeholder="Address"
               value={formData.address}
               onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded"
+              required
+              minLength={4}
             />
-            <input
-              type="date"
-              name="date"
-              value={formData.date}
-              onChange={handleChange}
-              className="w-full p-2 border border-gray-300 rounded"
-            />
+            <List className="grid grid-cols-2 2xl:grid-cols-4 bg-white rounded-lg">
+              {dates.map((date) => (
+                <ListItem key={date} className="p-0">
+                  <label
+                    htmlFor={date}
+                    className="flex w-full cursor-pointer items-center px-3 py-2"
+                  >
+                    <ListItemPrefix className="mr-3">
+                      <Radio
+                        name="date"
+                        value={date}
+                        onChange={handleChange}
+                        checked={formData.date === date}
+                        id={date}
+                        ripple={false}
+                        required
+                        className="hover:before:opacity-0"
+                        containerProps={{
+                          className: "p-0",
+                        }}
+                      />
+                    </ListItemPrefix>
+                    <Typography
+                      color="blue-gray"
+                      className="font-medium text-blue-gray-400"
+                    >
+                      {date}
+                    </Typography>
+                  </label>
+                </ListItem>
+              ))}
+            </List>
             <input
               type="time"
               name="time"
               value={formData.time}
+              required
               onChange={handleChange}
               className="w-full p-2 border border-gray-300 rounded"
             />
@@ -83,58 +268,13 @@ function Shipping() {
                 there is an issue with your order.
               </p>
             </div>
-            <button className="w-full bg-black text-white p-2 rounded mt-4" type="submit">
+            <button
+              className="w-full bg-black text-white p-2 rounded mt-4"
+              type="submit"
+            >
               SAVE & CONTINUE
             </button>
           </form>
-          <h2 className="font-julius lg:text-4xl md:text-4xl sm:text-3xl text-3xl mb-4 mt-4 text-gray-700">
-            02. PAYMENT
-          </h2>
-          <h2 className="font-julius lg:text-4xl md:text-4xl sm:text-3xl text-3xl mb-4 text-gray-700">
-            03. ORDER REVIEW
-          </h2>
-        </div>
-        <div className="w-full md:w-1/3 pl-0 md:pl-8 mt-8 md:mt-0">
-          <h2 className="font-julius lg:text-4xl md:text-4xl sm:text-3xl text-3xl mb-4 text-gray-700">
-            SUMMARY
-          </h2>
-          <div className="mb-4">
-            <div className="flex justify-between mb-2">
-              <span>Subtotal</span>
-              <span>$137.00</span>
-            </div>
-            <div className="flex justify-between mb-2">
-              <span>International Shipping</span>
-              <span>$18.00</span>
-            </div>
-            <div className="flex justify-between font-bold">
-              <span>Total</span>
-              <span>$155.00</span>
-            </div>
-          </div>
-          <h2 className="text-2xl font-bold mb-4">IN YOUR CART</h2>
-          <div className="space-y-4">
-            {[
-              { color: "Blue", size: "S", qty: 1, price: 59 },
-              { color: "Pink", size: "S", qty: 1, price: 39 },
-              { color: "Brown", size: "S", qty: 1, price: 39 },
-            ].map((item, index) => (
-              <div key={index} className="flex items-center">
-                <img
-                  src={`https://via.placeholder.com/50`}
-                  alt="Product"
-                  className="w-12 h-12 mr-4"
-                />
-                <div>
-                  <h3 className="font-bold">Breathable slim sport vest</h3>
-                  <p className="text-sm">Color: {item.color}</p>
-                  <p className="text-sm">Size: {item.size}</p>
-                  <p className="text-sm">Qty: {item.qty}</p>
-                  <p className="text-sm font-bold">${item.price}.00</p>
-                </div>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
       <Footer />
