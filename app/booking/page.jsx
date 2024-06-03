a7a07123cf41405a80b5ec7cac25f749ba117a44
atulvaru <atulk2512001@gmail.com>
2024-06-03 14:57:14 +0530

    booking pages

--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -1,9 +1,13 @@
-"use client"
+"use client";
+import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
-import React, { useEffect } from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 const Booking = () => {
-  const chechingAuthorization = async () => {
+  const [otp, setOtp] = useState("");
+
+  const checkingAuthorization = async () => {
     const id = localStorage.getItem("token");
     if (!id) {
       window.location.href = "/";
@@ -20,12 +24,125 @@ const Booking = () => {
       window.location.href = "/";
     }
   };
+
   useEffect(() => {
-    chechingAuthorization();
+    checkingAuthorization();
+    setOtp(Math.floor(1000 + Math.random() * 9000).toString());
   }, []);
+
   return (
     <div className="userpage-bg min-h-screen">
-      <Nav /> Booking
+      <Nav />
+      <div className="max-w-5xl bg-white bg-opacity-25 shadow-lg shadow-indigo-900/40 backdrop-blur-sm backdrop-filter backdrop-opacity-1 rounded-lg border border-opacity-20 border-white mx-auto my-8 bg-white p-6 rounded-xl shadow-lg">
+        <header className="mb-8 flex flex-col sm:flex-row items-center justify-center mx-auto gap-2">
+          <h1 className="font-julius text-center lg:text-4xl md:text-4xl sm:text-3xl text-3xl text-gray-700 font-bold">
+            Booking Details
+          </h1>
+        </header>
+
+        <section className="mb-8">
+          <div className="flex items-center gap-3">
+            <Image
+              src="/image/hero5.webp"
+              className="rounded-md"
+              alt="Booking"
+              width={96} // Adjust the width and height as needed
+              height={96}
+            />
+            <h3 className="font-julius lg:text-3xl md:text-2xl sm:text-2xl text-3xl text-gray-700 font-bold">
+              AC Installer
+            </h3>
+          </div>
+          <div className="flex flex-col sm:flex-row justify-between mt-2">
+            <div className="mb-4 sm:mb-0 leading-9">
+              <p>
+                Full Name: <strong className="text-gray-600">Atul Kumar</strong>
+              </p>
+              <p>
+                Phone: <strong className="text-gray-600">+91 9508973152</strong>
+              </p>
+              <p>
+                Address:{" "}
+                <strong className="text-gray-600">
+                  Bashist colony anishabad patna-800002
+                </strong>
+              </p>
+              <p>
+                Booking #: <strong className="text-gray-600">0000011</strong>
+              </p>
+              <p>
+                Booking Date:{" "}
+                <strong className="text-gray-600">02-29-2024</strong>
+              </p>
+              <p className="text-gray-800 font-bold flex items-center gap-2">
+                Status:{" "}
+                <strong className="    text-teal-500 rounded-md">
+                  Confirmed
+                </strong>
+              </p>
+            </div>
+            <div className="leading-9">
+              <p>
+                Available Date:{" "}
+                <strong className="text-gray-600">
+                  Sunday 11. August, 2024
+                </strong>
+              </p>
+              <p>
+                Timing:{" "}
+                <strong className="text-gray-600">10:00 AM - 02:00 PM</strong>
+              </p>
+              <p>
+                Quantity: <strong className="text-gray-600">1</strong>
+              </p>
+              <p className="text-gray-800 font-bold flex items-center gap-2">
+                Verification OTP:{" "}
+                <strong className="    text-teal-500 rounded-md">{otp}</strong>
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="mb-8">
+          <table className="min-w-full bg-white">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b font-julius lg:text-2xl md:text-xl sm:text-xl text-xl text-gray-700 font-bold">
+                  Summary
+                </th>
+                <th className="py-2 px-4 border-b text-right font-julius lg:text-2xl md:text-xl sm:text-xl text-xl text-gray-700 font-bold"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="py-2 px-4 border-b">Subtotal</td>
+                <td className="py-2 px-4 border-b text-right">₹600.00</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 border-b">Convenience Fee</td>
+                <td className="py-2 px-4 border-b text-right">₹18.00</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 font-bold">Total</td>
+                <td className="py-2 px-4 text-right font-bold">₹618.00</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+
+        <section className="mb-8">
+          <p className="font-bold text-red-600">
+            Note: Order can be cancelled up to 10 minutes before the scheduled
+            time.
+          </p>
+          <div className="flex justify-end">
+            <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded">
+              Cancel Order
+            </button>
+          </div>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 };
